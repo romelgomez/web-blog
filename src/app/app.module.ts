f67dfc10f83879c9216f1fecea0537455faba712
romelgomez <bmxquiksilver7185@gmail.com>
2017-12-11 23:24:40 -0400

    add basic routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
+// Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { RouterModule, Routes } from '@angular/router';
 
 // Material
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,17 +11,30 @@ import {
   MatSidenavModule,
   MatIconModule,
   MatButtonModule,
-  MatMenuModule
+  MatMenuModule,
+  MatListModule
  } from '@angular/material';
 // import { CdkTableModule } from '@angular/cdk/table';
 // import { FlexLayoutModule } from '@angular/flex-layout';
 
-
+// App
 import { AppComponent } from './app.component';
+import { MyResumeComponent } from './my-resume/my-resume.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { BlogComponent } from './blog/blog.component';
+
+const appRoutes: Routes = [
+  { path: 'my-resume',     component: MyResumeComponent },
+  { path: '',              component: BlogComponent },
+  { path: '**',            component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyResumeComponent,
+    PageNotFoundComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +44,12 @@ import { AppComponent } from './app.component';
     MatSidenavModule,
     MatIconModule,
     MatButtonModule,
-    MatMenuModule
+    MatMenuModule,
+    MatListModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
