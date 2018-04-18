401ac672f4702b1db8b202e2b82fb4de82ca07b1
Romel Gomez <bmxandcode@gmail.com>
2018-04-18 08:18:55 -0500

    add redirect after login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 // Dependencies to use the social logins in the component
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 import { Observable } from 'rxjs/Observable';
+import { ISubscription } from 'rxjs/Subscription';
 
 
 @Component({
@@ -11,15 +13,32 @@ import { Observable } from 'rxjs/Observable';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
   user: Observable<firebase.User>;
+  subscription: ISubscription;
 
-  constructor(private afAuth: AngularFireAuth) {
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router,
+  ) {
     this.user = afAuth.authState;
   }
 
+
   ngOnInit() {
+    this.subscription = this.user
+    .subscribe( user => this.islogged(user));
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  islogged (user) {
+    if ( user !== null) {
+      this.router.navigateByUrl('/account');
+    }
   }
 
 }
